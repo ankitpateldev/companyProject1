3657a413e4111a7722f871612fe7f956d9417375
ankitpateldev <patelfan7@gmail.com>
2023-03-31 13:00:06 +0530

    1 file modified, 5 added, 5 deleted

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './style.css';
-import Header1 from '../compo/Header1.js';
-import Panel from '../compo/Panel.js';
+import Header1 from './compo/Header1.js';
+import Panel from './compo/Panel.js';
 
-import RealNav from '../compo/RealNav.js';
-import AboveFooter from '../compo/AboveFooter.js';
-import Front from '../compo/Front.js';
+import RealNav from './compo/RealNav.js';
+import AboveFooter from './compo/AboveFooter.js';
+import Front from './compo/Front.js';
 
 export default function App() {
   return (
